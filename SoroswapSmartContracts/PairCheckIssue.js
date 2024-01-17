e17cebe55443096a452203da6735e361558fa267
coderipper <hello@joaquinsoza.dev>
2024-01-17 19:24:20 +0100

    update

---
+++ SoroswapSmartContracts/PairCheckIssue.js
@@ -0,0 +1,3 @@
+// Should try to create a pair directly to the factory contract and see what happens if the addresses given are the same
+// Then test again with different addresses 
+// Maybe check the rust tests? 
